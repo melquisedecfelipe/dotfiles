e963adea98dc566e4572c7dc8bb41e5e5aa44960
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2024-04-24 22:46:33 -0300

    Update installation script with gum scripts

--- install.sh
+++ install.sh
@@ -1,65 +1,139 @@
 #!/bin/bash
+brew_install() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install --quiet gum
+}
 
-message() {
-  echo -e "\n[INFO] $1"
+download() {
+  gum spin -s dot --title "Waiting, downloading $(style $1)..." -- curl -s -L -o $2 $3
+  info "$(style $1) downloaded successfully!"
 }
 
 error() {
-  echo -e "\n[ERROR] $1"
-  exit 1
+  log "error" "$1"
 }
 
-read -p "This script will install and configure tools. Do you want to continue? (y/n): " choice
-if [[ ! $choice =~ ^[Yy]$ ]]; then
-  message "Script aborted."
-  exit 0
-fi
+info() {
+  log "info" "$1"
+}
 
-message "Installing Homebrew..."
-if [[ "$(uname)" == "Darwin" ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || error "Failed to install Homebrew."
+fish_configure() {
+  if ! grep -q "/usr/local/bin/fish" /etc/shells; then
+    echo "/usr/local/bin/fish" | sudo tee -a /etc/shells || error "Failed to add fish to /etc/shells"
+  fi
+  chsh -s /usr/local/bin/fish || error "Failed to set fish as default"
+}
+
+git_configure() {
+  USERNAME=$(gum input --prompt "Your git $(style 'user.name'): " --value "Melquisedec Felipe")
+  EMAIL=$(gum input --prompt "Your git $(style 'user.email'): " --value "melquisedecfelipe@gmail.com")
+
+  git config --global user.name "$USERNAME"
+  git config --global user.email "$EMAIL"
+
+  git config --global core.editor "code --wait"
+
+  git config --global init.defaultBranch main
+
+  git config --global alias.cb "checkout -b"
+  git config --global alias.ci "commit -m"
+  git config --global alias.de "branch -D"
+  git config --global alias.lg "log --pretty=format:'%Cred%h%Creset %C(bold)%cr%Creset %Cgreen<%an>%Creset %s' --max-count=5"
+  git config --global alias.rollback "reset --soft HEAD~1"
+
+  info "$(style 'git') configured successfully!"
+}
+
+install() {
+  gum spin --spinner dot --title "Waiting, installing $(style $1)..." -- brew install "$1"
+  info "$(style $1) intalled successfully!"
+}
+
+install_all() {
+  echo $1 | tr " " "\n" | while read APP
+  do
+    install $APP
+  done
+}
+
+log() {
+  gum log --time TimeOnly --level $1 ": $2"
+}
+
+next() {
+  sleep 1
+  clear
+  echo "$(style ".dotfiles") next step"
+  gum style \
+    --align center --border normal --border-foreground 212 \
+    --margin "1" --padding "1 2" --width 40 "$1"
+}
+
+style() {
+  gum style --foreground 212 $1
+}
+
+warn() {
+  log "warn" "$1"
+}
+
+if [[ "$(uname)" == "Linux" ]]; then
+  echo "Linux detected, install requirements..."
+
+  if ! command -v brew &> /dev/null; then
+    brew_install
+  fi
+elif [[ "$(uname)" == "Darwin" ]]; then
+  echo "macOs detected, install requirements..."
+
+  if ! command -v brew &> /dev/null; then
+    brew_install
+  fi
 else
-  sudo apt-get update || error "Failed to update apt."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || error "Failed to install Homebrew."
+  echo "Unsupported OS!"
+  exit 1
 fi
 
-message "Installing fish shell..."
-brew install --quiet fish || error "Failed to install fish."
-if ! grep -q "/usr/local/bin/fish" /etc/shells; then
-  echo "/usr/local/bin/fish" | sudo tee -a /etc/shells || error "Failed to add fish to /etc/shells."
+next "Now, you ready! Welcome $(style '.dotfiles')"
+
+install "git"
+git_configure
+
+install "fish"
+fish_configure
+
+gum spin -s line --title "Install $(style 'Oh-My-Fish')..." -- curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+omf install nvm nai || error "Failed to install nvm and nai theme"
+
+info "$(style 'Oh-My-Fish') intalled successfully!"
+
+next "Common Apps"
+
+APPS=$(gum choose --no-limit --header "Select Common apps to install:" -- docker google-chrome postman slack spotify telegram visual-studio-code warp whatsapp 1password)
+
+if [ -n "$APPS" ]; then
+  install_all $APPS
+else
+  warn "You have not selected any options"
 fi
-chsh -s /usr/local/bin/fish || error "Failed to set fish as default."
 
-message "Installing Oh My Fish! and plugins (nvm and nai theme)..."
-curl -L https://get.oh-my.fish | fish || error "Failed to install oh-my-fish."
-omf install nvm nai || error "Failed to install nvm and nai."
-
-message "Installing Homebrew packages and casks..."
-brew install --quiet code git google-chrome postman spotify || error "Failed to install packages."
-brew install --quiet --cask warp || error "Failed to install casks."
 if [[ "$(uname)" == "Darwin" ]]; then
-  brew install --quiet --cask raycast zed || error "Failed to install casks on macOS."
+  next "MacOS Apps"
+
+  MACOS_APPS=$(gum choose --no-limit --header "Select only macOS apps to install:" -- arc notion-calendar raycast zed)
+
+  if [ -n "$MACOS_APPS" ]; then
+    install_all $MACOS_APPS
+  else
+    warn "You have not selected any options"
+  fi
 fi
 
-message "Configuring Git..."
-read -p "Enter your Git username: " git_username
-git config --global user.name "$git_username"
-read -p "Enter your Git email: " git_email
-git config --global user.email "$git_email"
+next "Assets"
 
-git config --global core.editor "code --wait"
+mkdir -p ~/Developer/Fonts
+download "JetBrains" ~/Developer/Fonts/JetBrainsMono-Regular.ttf https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
 
-git config --global init.defaultBranch main
-
-git config --global alias.cb "checkout -b"
-git config --global alias.ci "commit"
-git config --global alias.lg "log --pretty=format:'%Cred%h%Creset %C(bold)%cr%Creset %Cgreen<%an>%Creset %s' --max-count=30"
-git config --global alias.rollback "reset --soft HEAD~1"
-
-message "Downloading JetBrains font..."
-mkdir -p ~/Developer/Fonts || error "Failed to create font directory."
-curl -L -o ~/Developer/Fonts/JetBrainsMono-Regular.ttf https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip || error "Failed to download JetBrains font."
-curl -L -o ~/Developer/Fonts/JetBrainsMono-NF-Regular.ttf https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip || error "Failed to download JetBrains font."
-
-message "Installation complete."
-exit 0
+echo ""
+echo "Very well, installation $(style 'completed')!"
