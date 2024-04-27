e9e78724e427bf097ae5281201595b6581a8c9f6
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2024-04-27 13:41:43 -0300

    Update bootstrap

--- bootstrap-windows.bat
+++ bootstrap-windows.bat
@@ -1,20 +1,9 @@
 @echo off
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
-choco feature disable -n=allowGlobalConfirmation
+choco feature enable -n allowGlobalConfirmation
 
-choco install 1password
-choco install 7zip
-choco install brave
-choco install calibre
-choco install discord
-choco install epicgameslauncher
-choco install googlechrome
-choco install powertoys
-choco install spotify
-choco install steam
-choco install telegram
-choco install vlc
+choco install 1password 7zip brave calibre discord epicgameslauncher googlechrome opera-gx powertoys spotify steam telegram  vlc
 
-echo Bootstrap concluído.
-echo Remember to install Battle.net and Whatsapp.
+echo Very well, installation completed!
+echo Remember to install Battle.net and Whatsapp
