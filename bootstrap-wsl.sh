bf8240dffce6dfb3f36f1cbf0cf6ecd2e4f2179c
Melquisedec Felipe Coutinho <melquisedec.coutinho@mercadolivre.com>
2025-02-08 16:05:16 -0300

    Remove Kitty terminal from WSL bootstrap script

--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -10,7 +10,7 @@ install_homebrew() {
 }
 
 install_common() {
-    sudo apt update && sudo apt install -y git curl kitty fish
+    sudo apt update && sudo apt install -y git curl fish
 
     if ! grep -q "$(command -v fish)" /etc/shells; then
         echo "$(command -v fish)" | sudo tee -a /etc/shells
