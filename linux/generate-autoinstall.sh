ab59f3ab2dadb169c2f6b5f3d642f389cd9d1c91
Melquisedec Felipe Coutinho <melquisedec.coutinho@mercadolivre.com>
2025-02-08 15:45:10 -0300

    Refactor Linux autoinstall script and environment variables

--- linux/generate-autoinstall.sh
+++ linux/generate-autoinstall.sh
@@ -2,25 +2,17 @@
 
 set -euo pipefail
 
-if ! command -v mkpasswd &> /dev/null; then
-    sudo apt update && sudo apt install -y whois curl
-fi
+sudo apt update && sudo apt install -y whois curl python3
 
-if ! command -v python3 &> /dev/null; then
-    sudo apt update && sudo apt install -y python3
-fi
-
-if [ ! -f .env ]; then
-    exit 1
-fi
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source .env
 
-UBUNTU_PASSWORD=$(echo "$UBUNTU_PASSWORD" | mkpasswd -m sha-512 --stdin)
+PASSWORD=$(echo "$PASSWORD" | mkpasswd -m sha-512 --stdin)
 
-sed -e "s/realname: ''/realname: '$UBUNTU_REALNAME'/" \
-    -e "s/username: ''/username: '$UBUNTU_USERNAME'/" \
-    -e "s/password: ''/password: '$UBUNTU_PASSWORD'/" \
-    linux/autoinstall.template.yaml > linux/autoinstall.yaml
+sed -e "s/realname: ''/realname: '$REALNAME'/" \
+    -e "s/username: ''/username: '$USERNAME'/" \
+    -e "s/password: ''/password: '$PASSWORD'/" \
+    "$DIR/autoinstall.template.yaml" > "$DIR/autoinstall.yaml"
 
-cd linux && python3 -m http.server 8000
+cd "$DIR" && python3 -m http.server 8000
