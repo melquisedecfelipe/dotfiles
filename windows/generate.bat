8b8af48745de282d0da2c90ef5d28ee6fbf9ef52
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2025-02-19 23:01:01 -0300

    Improve Windows generate script with robust .env handling

--- windows/generate.bat
+++ windows/generate.bat
@@ -1,19 +1,21 @@
 @echo off
+setlocal enabledelayedexpansion
 
+cd /d "%~dp0"
 cd ..
 
-for /f "usebackq tokens=1,* delims==" %%a in (".env") do (
-    if "%%a"=="WINDOWS_USERNAME" set "WINDOWS_USERNAME=%%b"
+if not exist ".env" exit /b 1
+
+for /f "tokens=1,* delims==" %%a in (.env) do (
+    if "%%a"=="WINDOWS_USERNAME" (
+        set "WINDOWS_USERNAME=%%b"
+        set "WINDOWS_USERNAME=!WINDOWS_USERNAME:"=!"
+        set "WINDOWS_USERNAME=!WINDOWS_USERNAME: =!"
+    )
 )
 
-set WINDOWS_USERNAME=%WINDOWS_USERNAME:"=%
-set WINDOWS_USERNAME=%WINDOWS_USERNAME: =%
+if "%WINDOWS_USERNAME%"=="" exit /b 1
 
-if "%WINDOWS_USERNAME%"=="" (
-    echo WINDOWS_USERNAME not found
-    exit /b 1
-)
+powershell -Command "(Get-Content windows/autounattend.template.xml) -replace '\${WINDOWS_USERNAME}', '%WINDOWS_USERNAME%' | Set-Content windows/autounattend.xml"
 
-cd windows
-
-powershell -Command "(Get-Content autounattend.template.xml) -replace '\${WINDOWS_USERNAME}', '%WINDOWS_USERNAME%' | Set-Content autounattend.xml"
+endlocal
